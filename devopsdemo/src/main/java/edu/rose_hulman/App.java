a58883c17d1b4340e33c17a6622cfd51f66d8a7d
Stephen Acomb <6378907+Steven-Acomb@users.noreply.github.com>
2023-10-26 00:54:33 -0400

    Created getAppDescription and added Javadoc comments

--- devopsdemo/src/main/java/edu/rose_hulman/App.java
+++ devopsdemo/src/main/java/edu/rose_hulman/App.java
@@ -1,13 +1,52 @@
 package edu.rose_hulman;
 
 /**
- * Hello world!
+ * The {@code App} class represents an application with a name and a version number.
+ * It provides constructors to create instances of the application with various attributes.
  *
+ * This class can be used to model and manage application-related data and functionality.
+ *
+ * @version 1.0
+ * @since 2023-10-26
  */
-public class App 
+public class App
 {
+    private String appName;
+    private double version;
+
+    /**
+     * Constructs an {@code App} instance with the specified name and version.
+     *
+     * @param appName  The name of the application.
+     * @param version  The version number of the application.
+     */
+    public App(String appName, double version)
+    {
+        this.appName = appName;
+        this.version = version;
+    }
+
+    /**
+     * Gets the description of the application, including its name and version.
+     *
+     * @return A string representing the application's name and version.
+     */
+    public String getAppDescription()
+    {
+        String description = this.appName + ", version " + Double.toString(this.version);
+        return description;
+    }
+
+    /**
+     * The main method demonstrates the usage of the {@code App} class by creating an instance,
+     * getting the application description, and printing it to the console.
+     *
+     * @param args The command-line arguments (not used in this example).
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App myApp = new App("Test App", 1.0);
+        String description = myApp.getAppDescription();
+        System.out.println(description);
     }
 }
---
+++ devopsdemo/src/main/java/edu/rose_hulman/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package description goes here.
+ *
+ * You can provide additional details and explanations about the package here.
+ * This might include information about the purpose of the package, its contents,
+ * and how it should be used.
+ *
+ * @version 1.0
+ * @since 2023-10-26
+ */
+package edu.rose_hulman;
